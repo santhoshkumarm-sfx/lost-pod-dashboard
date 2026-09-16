de4861cf747c8665228085139b05cf36ea0c89dd
SHADOWFAX <santhoshkumar.m@shadowfax.in>
2026-09-16 12:58:24 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: '5mb' } },
-  eslint: { ignoreDuringBuilds: true },
-};
-export default nextConfig;
